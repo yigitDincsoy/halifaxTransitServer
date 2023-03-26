c5822d8b3cab59d988556248812a7c0ff6eef941
Yigit Dincsoy <w0471994@campus.nscc.ca>
2023-03-26 12:43:49 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
+
+var requestSettings = {
+  method: 'GET',
+  url: 'https://gtfs.halifax.ca/realtime/Vehicle/VehiclePositions.pb',
+  encoding: null
+};
+
+request(requestSettings, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
+     const myJSON = feed.toJSON();
+     console.log(typeof(myJSON))
+     console.log(myJSON.entity[0])
+
+
+  }
+});
