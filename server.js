e77bcc39e96d991fe1ce2d1dc5c75c3c191bc094
Yigit Dincsoy <w0471994@campus.nscc.ca>
2023-03-26 14:30:07 -0300

    ready-to-use

--- server.js
+++ server.js
@@ -1,11 +1,60 @@
-const express = require('express')
-const app = express()
-const port = 3000
+function getData(setup) {
+  const GtfsRealtimeBindings = require("gtfs-realtime-bindings");
+  const request = require("request");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  var requestSettings = {
+    method: "GET",
+    url: "https://gtfs.halifax.ca/realtime/Vehicle/VehiclePositions.pb",
+    encoding: null,
+  };
+
+  request(requestSettings, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const feed =
+        GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
+      busDataJSON = feed.toJSON();
+    //  console.log(myJSON.entity[0]);
+
+      if (setup) {
+      startServer();
+    } else {
+        setTimeout(getData, 5000);
+    }
+
+    // console.log("getData was called.")
+
+    }
+  });
+}
+
+function startServer() {
+
+  var corsOptions = {
+    origin: 'http://yigitdincsoy.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+  const express = require("express");
+  const app = express();
+  const cors = require('cors')
+  app.use(cors(corsOptions));
+  const port = 3000;
+
+  
+
+
+  app.get("/", (req, res) => {
+    res.send(busDataJSON);
+  });
+
+  app.listen(port, () => {
+    console.log(`Serving now on ${port}`);
+  });
+
+  setTimeout(getData, 9000);
+
+}
+
+let busDataJSON;
+getData(true);
